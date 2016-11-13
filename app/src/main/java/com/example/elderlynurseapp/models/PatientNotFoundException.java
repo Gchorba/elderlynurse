09d28eb4f7e5b604738a4eeef7b36fa8de4fd5a9
Gene Chorba <gchorba@gmail.com>
2016-11-13 01:49:35 -0500

    ALL of the sleeps

---
+++ app/src/main/java/com/example/elderlynurseapp/models/PatientNotFoundException.java
@@ -0,0 +1,23 @@
+package com.example.elderlynurseapp.models;
+
+/**
+ * An exception which is thrown when a patient is not found in an ER.
+ */
+public class PatientNotFoundException extends Exception {
+    /**
+     * Constructs this exception with no arguments.
+     */
+    public PatientNotFoundException() {
+        super();
+    }
+
+    /**
+     * Constructs this exception with the specified detail message.
+     *
+     * @param message the detail message for this exception
+     */
+    public PatientNotFoundException(String message) {
+        super(message);
+    }
+
+}
